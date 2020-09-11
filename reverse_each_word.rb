092306dcfd4b6c414b4664092ac610b4424dcb37
Leah Schlackman <leahemily1@gmail.com>
2020-09-11 17:03:46 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,11 @@ require 'pry'
 def reverse_each_word(phrase)
  # binding.pry
   arr = phrase.split(" ")
-  arr_two = arr.map(&:reverse)
-  arr_two.join(" ")
+  arr.map(&:reverse).join(" ")
 end
 
 def reverse_each_word(phrase)
  # binding.pry
   arr = phrase.split(" ")
-  arr_two = arr.collect(&:reverse)
-  arr_two.join(" ")
+  arr.collect(&:reverse).join(" ")
 end
